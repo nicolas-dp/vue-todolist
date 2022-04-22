433fe7f171d56c584da41171b40f5c6ebbd42b5a
Nicolas <wellcomenicolas@gmail.com>
2022-04-22 13:39:02 +0200

    MILESTONE 3 - Complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,7 @@ done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
 
 
- MILESTONE 3
- Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+ 
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) */
@@ -24,33 +23,33 @@ const app = new Vue({
 
     el: "#app",
 
-    data:{
+    data: {
         tasks: [
-            { 
+            {
                 text: 'Fare la spesa',
                 done: true
 
             },
 
-            { 
+            {
                 text: 'Passegiata con il cane',
                 done: false
 
             },
 
-            { 
+            {
                 text: 'Fare l esercizio to do list',
                 done: false
 
             },
 
-            { 
+            {
                 text: 'Venerdì bere con gli amici',
                 done: true
 
             },
 
-            { 
+            {
                 text: 'Andare al mare',
                 done: true
 
@@ -58,17 +57,25 @@ const app = new Vue({
         ]
     },
 
-/* MILESTONE 2
- Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
-  */
+    /* MILESTONE 2
+     Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+      */
     methods: {
-        removeTask(index){
+        removeTask(index) {
             //console.log(this.tasks);
-            this.tasks.splice(index,1)
+            this.tasks.splice(index, 1)
 
+        },
+        /* MILESTONE 3
+         Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti. */
+        addTask() {
+            console.log(this.tasks);
+            this.tasks.unshift({ text: this.newTask, done: false })
+            console.log(this.newTask);
+            this.newTask = ''
         }
     }
 
-    
+
 
 })
