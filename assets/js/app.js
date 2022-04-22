3c93ea7dad05ab6afe1b6f5f4c626828e3b72967
Nicolas <wellcomenicolas@gmail.com>
2022-04-22 14:37:54 +0200

    BONUS 1 & BONUS 2 - Complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,12 @@
 text, una stringa che indica il testo del todo
 done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
-
-
-
-
-
-
-
- 
-Bonus:
+BONUS:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) */
 
-
+/******** End description *******/
 
 //ogni todo sarà un oggetto, formato da due proprietà:text, una stringa che indica il testo del todo done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
@@ -68,11 +61,22 @@ const app = new Vue({
         },
         /* MILESTONE 3
          Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti. */
-        addTask() {
+        addTask(event) {
             console.log(this.tasks);
             this.tasks.unshift({ text: this.newTask, done: false })
             console.log(this.newTask);
             this.newTask = ''
+        },
+        /* BONUS
+        2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) */
+        
+        crossText(index) {
+            //console.log(this.tasks[index].done);
+                if (this.tasks[index].done == true) {
+                    this.tasks[index].done = false
+                } else if (this.tasks[index].done == false) {
+                    this.tasks[index].done = true
+                }
         }
     }
 
